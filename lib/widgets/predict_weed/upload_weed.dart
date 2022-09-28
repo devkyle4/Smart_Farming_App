669c2c22956bf6ff83702d6ccbccd9f72561c9b3
devkyle4 <73471060+devkyle4@users.noreply.github.com>
2022-09-28 02:34:44 +0000

    major changes

---
+++ lib/widgets/predict_weed/upload_weed.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Predict extends StatefulWidget {
+  @override
+  State<Predict> createState() => _PredictState();
+}
+
+class _PredictState extends State<Predict> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
